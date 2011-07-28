665e93fe20391d1a2b1a20d246d02fb779c7e7c0
apellew@gmail.com <apellew@gmail.com@577a2f5e-00a9-c898-8707-09bde1389247>
2011-07-28 20:30:58 +0000

    Version 0.2 - first release

---
+++ TotalCopy/frmMainForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TotalCopy
+{
+    public partial class frmMainForm : Form
+    {
+        public frmMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBeginCopy_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo Source = new DirectoryInfo(tbSourceDir.Text);
+            DirectoryInfo[] Targets;
+            Targets = new DirectoryInfo[tbTargetDirList.Lines.Count()];
+
+            int DirCount = 0;
+            foreach (string Line in tbTargetDirList.Lines)
+            {
+                Targets[DirCount] = new DirectoryInfo(tbTargetDirList.Lines[DirCount]);
+                DirCount = DirCount + 1;
+            }
+            file.CopyDirectory(Source, Targets, tbSourceDir.Text, tbMemoryDir.Text);
+        }
+
+        private void frmMainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            TotalCopy.Properties.Settings.Default.SourceDir = tbSourceDir.Text;
+            TotalCopy.Properties.Settings.Default.TargetDir = tbTargetDirList.Text;
+            TotalCopy.Properties.Settings.Default.MemoryDir = tbMemoryDir.Text;
+            TotalCopy.Properties.Settings.Default.Save();
+        }
+
+        private void frmMainForm_Load(object sender, EventArgs e)
+        {
+            tbSourceDir.Text = TotalCopy.Properties.Settings.Default.SourceDir;
+            tbTargetDirList.Text = TotalCopy.Properties.Settings.Default.TargetDir;
+            tbMemoryDir.Text = TotalCopy.Properties.Settings.Default.MemoryDir;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TotalCopy/library/array.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TotalCopy.library
+{
+    class array
+    {
+        public static void InsertIntoArray(Array target,
+      object value, int index)
+        {
+            if (index < target.GetLowerBound(0) ||
+                index > target.GetUpperBound(0))
+            {
+                throw (new ArgumentOutOfRangeException("index", index,
+                  "Array index out of bounds."));
+            }
+            else
+            {
+                Array.Copy(target, index, target, index + 1,
+                           target.Length - index - 1);
+            }
+
+            target.SetValue(value, index);
+        }
+
+        public static void RemoveFromArray(Array target, int index)
+        {
+            if (index < target.GetLowerBound(0) ||
+                index > target.GetUpperBound(0))
+            {
+                throw (new ArgumentOutOfRangeException("index", index,
+                  "Array index out of bounds."));
+            }
+            else if (index < target.GetUpperBound(0))
+            {
+                Array.Copy(target, index + 1, target, index,
+                           target.Length - index - 1);
+            }
+
+            target.SetValue(null, target.GetUpperBound(0));
+        }
+    }
+}
